a2be7750225d99fe939182d5336e4ccee590876b
Tudor Popa <tudorp9k@gmail.com>
2023-05-09 20:18:13 +0300

    Initial VS project

---
+++ MusicPlatform/MusicPlatform/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicPlatform
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
