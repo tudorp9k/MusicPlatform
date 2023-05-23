d8017a74a6ddbf1823e60e41c9d97ca3e7569b26
Tudor Popa <tudorp9k@gmail.com>
2023-05-23 20:20:59 +0300

    Added DbSets to context class

--- MusicPlatform/MusicPlatform.DataLayer/MusicDbContext.cs
+++ MusicPlatform/MusicPlatform.DataLayer/MusicDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MusicPlatform.DataLayer.Models;
 using System.Configuration;
 
 namespace MusicPlatform.DataLayer
 {
     internal class MusicDbContext : DbContext
     {
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["MusicPlatform"].ConnectionString);
